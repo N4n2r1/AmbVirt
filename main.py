5dd8ae23d175c03c88088a12796c57a3886f9bb9
Riana <riana.aldias@gmail.com>
2026-03-06 10:05:23 -0300

    .

--- main.py
+++ main.py
@@ -8,3 +8,9 @@ def index():
 
 if __name__ == "__main__":
     app.run()
+
+@app.route("/curso")
+def curso():
+    return "<h1>Eu faço curso de Técnio em Desenvolvimento de Sistemas!</h1>"
+if __name__ == "__main__":
+    app.run()
